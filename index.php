caeb5597cbc336b5f36c7acf5eacde2ec074d100
abdullah19992000 <83210637+abdullah19992000@users.noreply.github.com>
2021-08-18 12:28:51 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Banking System</title>
+  </head>
+
+  <body style="background-color: black;">
+  <header>
+  <?php
+  include 'navbar.php';
+  ?></header>
+<main>
+  <style>
+  body{
+    background-image: url('img/myyy.jpg');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    width: 100%;
+    background-position: 210px 70px;
+  }
+</style>
+<span style = "position:relative; left:80px; top:80px; color:yellow;  font-family: Comic Sans MS;">W</sapn>
+<span style = "position:relative; left:90px; top:80px; color:yellow;  font-family: Comic Sans MS;">E</span>
+<span style = "position:relative; left:90px; top:80px; color:yellow;  font-family: Comic Sans MS;">L</span>
+<span style = "position:relative; left:85px; top:80px; color:yellow;  font-family: Comic Sans MS;">C</span>
+<span style = "position:relative; left:85px; top:80px; color:yellow;  font-family: Comic Sans MS;">O</span>
+<span style = "position:relative; left:80px; top:80px; color:yellow;  font-family: Comic Sans MS;">M</span>
+<span style = "position:relative; left:88px; top:80px; color:yellow;  font-family: Comic Sans MS;">E</span>
+    </main>
+  </body>
+</html>
